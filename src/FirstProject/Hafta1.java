dd773de6411e7a28f99f9989a33c24810f085952
mathinformatics <mathinformatics@iMac-4.lan>
2022-10-09 19:39:17 -0400

    Hafta 1-2-3-4 notlari eklendi

---
+++ src/FirstProject/Hafta1.java
@@ -0,0 +1,70 @@
+package FirstProject;
+
+public class Hafta1 {
+
+//IDE: ItelliJ IDEA =  text editor  + Compiler +  terminal
+
+    public static void main(String[] args) {
+
+
+
+
+        //variable:
+
+
+        // math
+
+        // data structures + algorithms = programming
+
+
+        //syntax rules
+        //ctrl + c : copy
+        //ctrl + x : cut
+        //ctrl + v : paste
+
+
+        //back slash: escape character
+
+      //  System.out.println("Hello World");
+      //  System.out.println(" AAAA!Ferid Dediki \"Bunlar cok basit.\" ");
+
+       // System.out.print("2+5 = 7");
+
+
+        //task1: ekrana 2+5 = 7  in sonucunu yazdirin.}
+
+
+     //System.out.println(34);
+
+    //System.out.println();
+
+
+        //System.out.println(345);
+
+        //System.out.println(4+5);
+
+        // 3 + 5 = 8
+        // 3 + x = 3 + x
+/*
+        System.out.println("2+3 = " + (2 + 3) );
+        System.out.print( 2 + 3 + " = 2 + 3");
+        System.out.println( "hello");
+*/
+        //   "2 + 3 = " +  5
+
+        //  + : iki manasi var:
+        // birincisi toplama yaptirir. sayilari toplatir.  sayi + sayi ---> toplar
+        // ikincisi: beraber yazdirir. Birlestirir.  text ile sayiyi.  text + sayi --> birlestirir
+
+        // 4 + 5 - 6 =
+
+        // (4 + 5) * 6 =
+
+        // Q and A
+        // Q & A
+        //    ` = back tick
+
+    }
+
+
+}
---
+++ src/FirstProject/Hafta4.java
@@ -0,0 +1,115 @@
+package FirstProject;
+
+public class Hafta4 {
+
+
+    public static void main(String[] args) {
+
+        //Recap: Tekrar: Haft1-2-3
+
+        // single line comment is double forward slash: // and multi-line comment is pair of /* and */
+
+
+
+        //ornek:
+        //    x + 3 = 5
+
+
+        //ornek:
+
+ /*       arithmetic : sayilar ve islemler
+        algebra: artihmetic + bilinmeyen (x,y,z, ...)
+        x + 3 = 8
+       */
+
+
+
+        //auto-indent can be used to indent your code to be easily readable
+
+        //println() vs print():
+        //println(argumanet): ekranan argumanini yazdiriyor. cursor bir sonraki line a gider ve durur.
+        //print(arguman): ekranan argumanini yazdiriyor.  mesajin bittigi yerde durur.
+
+
+
+        //how to print string and numbers:
+        //sout("string")
+        //sout(sayi)
+
+
+        //printing strings and numbers:
+        //sout( 3 + "hello") --> output: 3hello
+
+
+        // islem onceligi:   ussu sayilar, parantez, carpma ve bolme , toplama ve cikarma.
+        //ayni anda (toplama ve cikarma) veya (carpma ve bolme) olursa  soldan dogru basliyarak islemleri yapariz.
+        //sout(" 3 * 1 = " + 3 * 1);
+
+       // System.out.println(" 3 + 1 = " + 3 + 1);    // output:   "3 + 1 = 3 " + 1  --->   3 + 1 = 31
+       // System.out.println(" 3 + 1 = " + (3 + 1));  // output:   "3 + 1 = " + 4   --->
+
+
+        //code sharing: github , bitbucket, gitlab, etc
+
+
+
+        //dev team: 4 developer , 3 tester,  1 PM, 1 PO
+        //daily standup : dun ne yaptim, bugun ne yapicam, blocker'im varmi
+        //team player : iletisime acik mi
+
+        //reboot your pc: cogu sorunun cozumu.
+
+
+
+
+        //VCS: Version Control System: git, mercurial, subversion, etc
+        //10 satir: run edip hata varmi yok mu bakarsin.--> hata yoksa --> VCS programina kaydediyorum.
+        //20 satir ekledik:
+        //30 satir
+        //20 satir
+        //run ettik: hata verdi . onemli olan hatayi en kisa surede bulmak. progrmai tekrar aclisir hale getirmek.
+
+        //hesap makinesi 1.0.0        4 islem: +, -, *, /
+        //hesap makinesi 1.1.0        4 islem + modulos islem  5 % 3 = 2 bolme isleminda kalan sayiyi veriyor.
+        //hesap makinesi 1.1.1     patch: yama yapiyor.
+
+        //github ile bir merkezi yere ekliyoruz.
+        //takim olarak biri  github da projeyi olusturuyor.
+        //yada kendi bilgisayarindaki projeyi github da yayinliyo.
+
+
+      //  System.out.println("gas station");
+     //   System.out.println("3lb 3 $");
+
+
+
+
+        System.out.println("gas station " +
+                          "  3lb 3 $ " +
+                          "adres: " +
+                          "tarih: ");
+
+
+        System.out.println("gas station \n 3lb 3$");
+
+
+
+
+
+        //high to low level languages
+        //blockly : strach
+        //python ve javascript
+
+        // java, c#
+
+        //c
+
+        //assembly
+
+    }
+
+
+
+
+
+}
