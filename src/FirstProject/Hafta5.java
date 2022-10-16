ee025f0da77d99d78d30c5f9a8613bb5fb4229ea
cmert2017 <cihanmert2075@gmail.com>
2022-10-16 07:38:46 -0400

    Hafta6 ilk konular

---
+++ src/FirstProject/Hafta5.java
@@ -0,0 +1,26 @@
+package FirstProject;
+
+public class Hafta5 {
+
+    public static void main(String[] args) {
+
+        System.out.println("5+2");
+        System.out.println(5+2);
+
+        // 5 + 2 =  7
+        System.out.println("5+2 = " + 5 + 2);
+        System.out.println("5+2 = " + (5 + 2));
+        System.out.println("3 * 1 = " + (3 * 1));
+
+       // "5 + 2 = 5" + 2
+        //"5 + 2 = 52"
+
+
+
+    }
+
+
+    //intellisense im caismiyor.
+
+
+}
---
+++ src/FirstProject/Hafta6.java
@@ -0,0 +1,12 @@
+package FirstProject;
+
+public class Hafta6 {
+
+
+    //best coding practices
+    //1- parentez blocklari yeni line dan baslar
+
+
+
+
+}
