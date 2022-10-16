fd48911c32c690e18895a0c60d8df9db121962ff
cmert2017 <cihanmert2075@gmail.com>
2022-10-16 08:15:01 -0400

    variable tanimlama

--- src/FirstProject/Hafta5.java
+++ src/FirstProject/Hafta5.java
@@ -4,23 +4,61 @@ public class Hafta5 {
 
     public static void main(String[] args) {
 
+        //intellisense im caismiyordu bende intelli J idea mi yeniden baslattim ve problem cozuldu.
+
+        //best coding practices
+        //1- parentez blocklari yeni line dan baslar
+        
+
         System.out.println("5+2");
         System.out.println(5+2);
+        System.out.println(5);
 
         // 5 + 2 =  7
         System.out.println("5+2 = " + 5 + 2);
         System.out.println("5+2 = " + (5 + 2));
+        System.out.println("******carpim tablosu 3lere ait*********");
         System.out.println("3 * 1 = " + (3 * 1));
+        System.out.println("3 * 2 = " + (3 * 2));
+        System.out.println("3 * 3 = " + (3 * 3));
+        System.out.println("3 * 4 = " + (3 * 4));
+        System.out.println("3 * 5 = " + (3 * 5));
+        System.out.println("3 * 6 = " + (3 * 6)); //burdaki parentezler gereksiz yani redundant 
+        System.out.println("3 * 7 = " + 3 * 7);
+        System.out.println("3 * 8 = " + 3 * 8);
+        System.out.println("3 * 9 = " + 3 * 9);
+        System.out.println("3 * 10 = " + 3 * 10);
 
        // "5 + 2 = 5" + 2
         //"5 + 2 = 52"
 
 
 
+
+        //windows: snagit ile ekran goruntusu alabilirsiniz ve uzerinde oynayabilirisniz
+        //cmd + shift + 5 ile ekran goruntusu alacaginiz programi cagirip ekran goruntusu alabilirisniz
+
+
+
+        //variables: bilinmeyen kullanma
+        System.out.println("*********Variables**************");
+
+        //declaration of a variable
+        String companyName = "First Brick International";
+        System.out.println(companyName);
+
+        System.out.println(companyName + " is an international organization");
+        System.out.println("There are over 500 teachers in " + companyName);
+        
+        
+        //PO: Product owner: artik sirketin Frit Brick degil First Brisk International. Bunu programmda degistirin.
+        
+
+
     }
 
 
-    //intellisense im caismiyor.
+   
 
 
 }
--- src/FirstProject/Hafta6.java
+++ src/FirstProject/Hafta6.java
@@ -3,8 +3,18 @@ package FirstProject;
 public class Hafta6 {
 
 
-    //best coding practices
-    //1- parentez blocklari yeni line dan baslar
+
+
+    public static void main(String[]  args) {
+
+
+
+
+
+
+    }
+
+
 
 
 
