2b81a167a0f2a614b91b170e0400b6a575bf0d4f
cmert2017 <cihanmert2075@gmail.com>
2022-11-13 07:41:36 -0500

    HAfta9 ders konulari: Exercise about IF

--- src/FirstProject/Hafta9.java
+++ src/FirstProject/Hafta9.java
@@ -1,5 +1,7 @@
 package FirstProject;
 
+import java.util.Scanner;
+
 public class Hafta9 {
 
 
@@ -17,6 +19,34 @@ public class Hafta9 {
         //task1:condition:  https://www.codingame.com/training/easy/power-of-thor-episode-1
         //task2: condition: https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
 
+
+        //even or odd number
+
+        Scanner input = new Scanner(System.in);
+        String result;
+        int girilenSayi;
+
+
+        //input
+        System.out.println("Bir sayi girin: ");
+        girilenSayi = input.nextInt();
+
+
+
+        //process: even or odd
+        if(){
+           result = "Even";
+        }
+
+        if(){
+
+            result = "Odd";
+        }
+
+
+        //output
+
+        System.out.println("Girdiginiz sayi: " + result);
 
 
 
