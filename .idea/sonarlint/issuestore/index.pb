656be0714189b6b9409f3985f2197995e20c5be3
cmert2017 <cihanmert2075@gmail.com>
2022-11-13 07:33:02 -0500

    HAfta9 ders konulari: Calculator progrmai eklendi

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+G
+src/Kutuphane/Book.java,c/7/c7e0ecdf2b3bbe628da3087de1f038328771bcb5
+I
+src/Muhasebe/Student.java,9/4/94c1ba087ee58b0e94d12fd541af3ed4589b543a
+K
+src/FirstProject/Books.java,5/d/5dc73c26fcdee421327ef9dac9f1896f0b3473a2
+M
+src/FirstProject/Clients.java,4/0/40259048398471ccd7817c3f29b908f6f249854c
---
+++ src/FirstProject/Hafta9.java
@@ -0,0 +1,24 @@
+package FirstProject;
+
+public class Hafta9 {
+
+
+    public static void main(String[] args) {
+
+        boolean x = false, z = true;
+        int y = 20;
+        x =  (y!=10) || (z==false);
+
+        //Guess what will be the value of x?
+
+        System.out.println("x = " + x);
+
+        //task0:  variable assignment: https://www.codewars.com/kata/50ee6b0bdeab583673000025
+        //task1:condition:  https://www.codingame.com/training/easy/power-of-thor-episode-1
+        //task2: condition: https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+
+
+
+    }
+}
