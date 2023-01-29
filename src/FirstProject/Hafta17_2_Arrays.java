7af2d9f7b3e8cf9e4f9b455026f32ddbcf5844fe
cmert2017 <cihanmert2075@gmail.com>
2023-01-29 13:52:22 -0500

    HAfta17 ders konulari: nested loops, Array

--- src/FirstProject/Hafta17_2_Arrays.java
+++ src/FirstProject/Hafta17_2_Arrays.java
@@ -42,7 +42,7 @@ public class Hafta17_2_Arrays {
 
         //yukardaki tekrardan kurtulmak icin array ile alakali soyle bir yontem gelistirmisler
 
-        int[] matScores2 = {100,90,100,50,100,100,10,40,80,100};
+        int[] matScores2 = {80,90,100,50,130,100,10,40,80,100};
 
 
         //matematiksel cozum:
@@ -66,7 +66,21 @@ public class Hafta17_2_Arrays {
 
         //2.a. max score bulma
 
-        
+        //matematiksel:
+        // ilk sayiya max diyecez.
+        //daha sonra onu tek tek tum sayilarla karsilastiracaz.
+        int max = matScores2[0];
+
+        for (int i = 1; i < matScores2.length; i++) {
+
+            if(max < matScores2[i]){
+                max = matScores2[i];
+            }
+
+        }
+
+        System.out.println("max = " + max);
+
 
 
         //2.b min score u bulma
