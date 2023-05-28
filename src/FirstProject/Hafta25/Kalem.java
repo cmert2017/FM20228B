784ae9dda29ab60e20bd792a310760f0e2c2a340
cmert2017 <cihanmert2075@gmail.com>
2023-05-28 13:27:09 -0400

    created kalem class

--- src/FirstProject/Hafta25/Kalem.java
+++ src/FirstProject/Hafta25/Kalem.java
@@ -2,6 +2,27 @@ package FirstProject.Hafta25;
 
 public class Kalem {
 
+    //computer = hardware  +  software
+    //hardware: CPU(beyin), Hard Disk (Kalici hafiza), RAM(gecici hafiza),motherboard(vucud iskelet),
+    //By clicking save button we transfer the things from RAM to HArd disk.
+    //humans can move new things from RAM to Harddisk by making relations between.
+    //new knowledge will be stores in snapses.
+    //A snapses is a connection between two nerons(brain cell).
+    //it is all greek to me!
+    //iskelet'e organlari koyuyoruz ve onlari sinir sustemi ile bagliyoruz haberlesmeleri icin.
+    //motherboard'a da CPU, RAM'i koyuyoruz(nasil ekliyoruz by soldering). bunlar birbirine wires(circuit) ile bagli.
 
+
+    //we can use our computer via terminal or by GUI: graphical user interface
+    //terminal works with commands
+    //GUI also uses terminal commands but you dont need to write them. Automatically they are used in the background.
+
+
+
+
+    //we  want to manage our local code. We an do it with GIT.
+    //GIT is an application.
+    //GIT has three stages: working, staging index, repository
+    //use git add to take file from working to staging index.
 
 }
